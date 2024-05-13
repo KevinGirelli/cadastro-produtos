c8e90261ad93d60f82eca4c89c1c59fa36e52e2e
Kevin Girelli <kevingirelli.gg@gmail.com>
2024-05-13 14:40:01 -0300

    Api Backend

--- frontend/angular_files/src/app/app.routes.ts
+++ frontend/angular_files/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CadastroProdutoComponent } from './components/cadastro-produto/cadastro-produto.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
   },
-  
+  {
+    path: '/cadastro',
+    component: CadastroProdutoComponent
+  }
 ];
